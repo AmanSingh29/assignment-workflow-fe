f3c0162ee5d3a87aacb1fa22aeed7c0d2900f6c8
Aman Singh <amansingh979429@gmail.com>
2025-12-27 08:40:38 +0530

    toast setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,23 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </AuthProvider>
   </BrowserRouter>
 );
